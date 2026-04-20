ae31354e7410990afbf11288416c695e62986517
cesarparazi <ceparazi@gmail.com>
2026-04-20 10:07:07 +0000

    Inserido o código inicial

---
+++ concat_dados.py
@@ -0,0 +1,7 @@
+#Receber dois dados diferentes do usuário e concatenar os valores, mostrando o resultado final com um espaço em branco entre eles.
+    
+
+dados1 = input("Digite o primeiro dado: ")
+dados2 = input("Digite o segundo dado: ")
+resultado = dados1 + " " + dados2
+print("O resultado da concatenação é:", resultado)
