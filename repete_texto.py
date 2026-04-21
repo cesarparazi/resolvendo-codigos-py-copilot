c1089749a5967cd403268c3303074e71e7062c55
cesarparazi <ceparazi@gmail.com>
2026-04-21 15:24:01 +0000

    inserção do código fonte

---
+++ repete_texto.py
@@ -0,0 +1,6 @@
+# Solicita uma string e um número inteiro como entrada
+string = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+# Repete a string o número de vezes especificado
+print((string + " ") * numero)
